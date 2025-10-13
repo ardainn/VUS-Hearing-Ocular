8fbecd3bb6aaa01e37e8e3c797029d6afd938eb8
ardainn <100212797+ardainn@users.noreply.github.com>
2025-10-13 19:25:55 +0300

    add round_percent function

--- scripts/table1.R
+++ scripts/table1.R
@@ -1,4 +1,59 @@
 library(data.table)
+library(scales)
+
+# function to round to 100
+round_percent <- function(x) {
+  p <- x / sum(x) * 100
+  p_floor <- floor(p)
+  remainder <- round(100 - sum(p_floor))
+  
+  # add 1 to the largest fractional parts
+  if (remainder > 0) {
+    idx <- order(p - p_floor, decreasing = TRUE)[1:remainder]
+    p_floor[idx] <- p_floor[idx] + 1
+  }
+  return(p_floor)
+}
+
+# function to generate sum table
+make_wide_summary <- function(data = cleaned_data, row_var, col_var = "disease_domain") {
+  
+  # count rows per row_var x col_var
+  df <- data[!is.na(get(col_var)),
+             .N,
+             by = c(row_var, col_var)]
+  
+  # pivot to wide format
+  wide_df <- dcast(data = df, formula = as.formula(paste(row_var, "~", col_var)),
+                   value.var = "N", fill = 0)
+  setDT(wide_df)
+  
+  wide_df[, Total := H + `H+V` + V]
+  
+  # identify numeric columns (domains)
+  domain_cols <- setdiff(names(wide_df), row_var)
+  for (col in domain_cols) wide_df[[col]] <- as.numeric(wide_df[[col]])
+  
+  # compute column totals
+  col_totals <- sapply(domain_cols, function(x) sum(wide_df[[x]], na.rm = TRUE))
+  
+  # format counts with column-wise percentages
+  pretty_df <- copy(wide_df)
+  for (col in domain_cols) {
+    pretty_df[[col]] <- paste0(round_percent(wide_df[[col]] / col_totals[col]),"%")
+  }
+   
+  return(pretty_df)
+}
+
+
+tab1 <- make_wide_summary(row_var = "gender")
+tab2 <- make_wide_summary(row_var = "leadership")
+tab3 <- make_wide_summary(row_var = "professional_position")
+tab4 <- make_wide_summary(row_var = "years_professional_experience")
+
+
+library(data.table)
 
 make_wide_summary <- function(data = cleaned_data, row_var, col_var = "disease_domain") {
   
@@ -36,6 +91,7 @@ tab1 <- make_wide_summary(row_var = "gender")
 tab2 <- make_wide_summary(row_var = "leadership")
 tab3 <- make_wide_summary(row_var = "professional_position")
 tab4 <- make_wide_summary(row_var = "years_professional_experience")
+
 
 
 
