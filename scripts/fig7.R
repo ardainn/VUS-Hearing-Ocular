c35940851e78d67a7b811fbed3aa197b0e4ab1ac
ardainn <100212797+ardainn@users.noreply.github.com>
2025-10-13 18:01:15 +0300

    Update fig7.R

--- scripts/fig7.R
+++ scripts/fig7.R
@@ -1,49 +1,69 @@
 # get survey data and helper functions
-source("../01_config.R")
-source("../02_helpers.R")
+source("_config.R")
+source("_helper.R")
 
 # define plot colors
-colors <- c("#ff9f1c", "#ffbf69", "#ffffff", "#8BE4DE", "#2ec4b6")
+colors <- c("#DCCABC",
+            "#CEB5A1",
+            "#C0A087",
+            "#B28C6C",
+            "#A07754",
+            "#866346")
 
 # define and map labels
 labs <- c(
-  "strongly disagree",
-  "disagree",
-  "neither agree nor disagree",
-  "agree",
-  "strongly agree"
+  "unaware of the resource",
+  "not confident at all",
+  "slightly confident",
+  "somewhat confident",
+  "confident",
+  "very confident"
 )
-likert_map <- setNames(seq_along(labs), labs)
+likert_map <- setNames(0:(length(labs) - 1), labs) # starts from 0
 
-#### CLIN UT IMPROVEMENTS ####
+#### FUNC EV GUIDELINES ####
 
-cols_ci <- 108:112
+# define cols
+cols_fg <- 71:80
 
-colnames(cleaned_data)[108:112] <- c(
-  "Literature supports clinical use of variant-level functional data",
-  "Literature supports clinical use of variant-level MAVE data",
-  "More studies needed to prove clinical use of variant-level functional data",
-  "More studies needed to prove clinical use of variant-level MAVE data",
-  "ClinVar should clarify clinically relevant transcripts"
+# update labels
+colnames(cleaned_data)[cols_fg] <- c(
+  "ACMG/AMP Guidelines",
+  "ClinGen SVI WG Functional Evidence Guidelines (PS3/BS3)",
+  "ClinGen SVI WG Splicing Evidence Guidelines",
+  "ClinGen VCEP-specific Guidelines for Functional Evidence Use",
+  "ClinGen SVI WG Functional Assay Assessment Worksheet",
+  "ClinGen Specifications of ACMG/AMP Guidelines for Genetic Hearing Loss",
+  "ClinGen HL Expert Panel Specifications v2.0",
+  "ClinGen HL Expert Panel Specifications v1.0",
+  "ClinGen LCA/EORD Expert Panel Specifications v1.0",
+  "ClinGen Glaucoma Expert Panel Specifications v1.1"
 )
 
-# define likert df
-likert_df_ci <- make_likert_df(cleaned_data, cols_ci, labs)
+# copy cleaned_data
+cleaned_data_fg <- cleaned_data
+
+# for V group, set questions 6-8 to NA
+cols_V <- 76:78 
+cleaned_data_fg[cleaned_data_fg$disease_domain == "V", cols_V] <- NA
+
+# for H group, set questions 9-10 to NA
+cols_H <- 79:80 
+cleaned_data_fg[cleaned_data_fg$disease_domain == "H", cols_H] <- NA
+
+# make Likert dataframe
+likert_df_fg <- make_likert_df(cleaned_data_fg, cols_fg, labs)
 
 # plot
 plot_likert(
-  likert_df_ci,
+  likert_df_fg,
   colors = colors,
-  file_name = "likert_clinut_impr.png",
-  include_center = F,
-  legend_title_centered = T,
-  legend_break = T,
-  height = 23 / 2
+  file_name = "likert_funcev_gdln.png",
+  include_center = T,
+  add_missing_center_perc = T,
+  center_col = 4,
+  height = 23
 )
 
 # run chisq test
-results_ci <- run_chisq(likert_df_ci, likert_map)
-# run chisq test
-results_dv <- run_chisq(likert_df_dv, likert_map)
-
-
+results_fg <- run_chisq(likert_df_fg, likert_map)
